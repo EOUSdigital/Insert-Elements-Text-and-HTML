88e370eb4804249e56dc12875a9d79eaae4c6fac
EOUSdigital <117541034+EOUSdigital@users.noreply.github.com>
2026-01-10 19:14:12 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+"use strict";
+
+//TODO  🟦 Module 7 - DOM Manipulation: Lesson 11. Insert Elements, Text and HTML
+
+//  This lesson follows directly after “Refactor To Multiple Functions” and focuses on inserting existing elements, plain text, and HTML strings into precise positions in the DOM.
+
+//* What you are learning
+//  By the end, you should be able to:
+//  • Insert content before/after an element, and at the start/end of an element.
+//  • Choose the right API for the job:
+//      • Elements → before(), after(), prepend(), append(), insertAdjacentElement()
+//      • Text → textContent, insertAdjacentText()
+//      • HTML → insertAdjacentHTML() (careful: security)
+//  • Explain when to use textContent instead of innerHTML (most of the time).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
